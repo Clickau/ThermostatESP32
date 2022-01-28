a24e2b255d60968126bd26531ecba373b5ca3649
Clickau <firmabugiii@gmail.com>
2022-01-28 21:21:50 +0200

    Change Firebase Root CA; bump version to 1.0.1

--- main/include/version.h
+++ main/include/version.h
@@ -1,4 +1,4 @@
-const char *VERSION_STRING = "1.0.0";
+const char *VERSION_STRING = "1.0.1";
 const int   VERSION_MAJOR  = 1;
 const int   VERSION_MINOR  = 0;
-const int   VERSION_PATCH  = 0;
+const int   VERSION_PATCH  = 1;
